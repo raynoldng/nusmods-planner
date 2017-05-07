b5548adc6d528f945c2a03cda85dbfb76af2d8e3
Raynold Ng <raynold.ng24@gmail.com>
2017-05-07 23:09:25 +0800

    Removed unnecessary lines and added more comments

--- api_test.py
+++ api_test.py
@@ -5,9 +5,6 @@ from z3 import *
 
 ENV = "DEV" # faster to do everything offline, AY16-17S
 
-# ModuleCode -> [Lessons]
-
-## Helper functions
 if ENV == "DEV":
     _mods = json.load(open('timetable.json'))
     _dict = {x['ModuleCode']: x['Timetable'] for x in _mods if 'Timetable' in x}
@@ -26,9 +23,9 @@ def splitIntoLessonTypes(mod):
     		mydict[lType][classId] = mydict[lType][classId] + tList
     	else:
     		mydict[lType][classId] = tList
-    #dictionary(lessontype,dictionary(classNo, timeList))
     return mydict
 
+# Sample API call:
 # http://api.nusmods.com/2016-2017/1/modules/ST2131/timetable.json
 # returns tuple of (ModuleCode, [{Lessons for each type}])
 def query(code):
@@ -43,13 +40,14 @@ def query(code):
 	r = r.json()
 	return r
 
-# returns free day constraint, x is a weekday from 0 to 4
+# returns list of hours corresponding to day of the week for even and odd week
 def freeDay(x):
 	day = range(x*24,(x+1)*24)
 	return day + [i+120 for i in day]
 
 # returns list of discrete timeslots based on hour-based indexing in a fortnight
 # used for z3's distinct query. 0-119 first week, 120-239 second week.
+# 24 hours in a day
 def timeList(weektext, daytext, starttime, endtime):
     #some hard code
     weekdays = {"Monday": 0, "Tuesday": 1, "Wednesday": 2, "Thursday": 3, "Friday": 4}
@@ -63,68 +61,62 @@ def timeList(weektext, daytext, starttime, endtime):
     else:
         return [i+120 for i in lst]+lst
 
-def transformMod(modtuple):
-    return (modtuple[0], splitIntoLessonTypes(modtuple[1]))
 
-# list of (moduleCode, {transformedLessons}) tuples and returns imcomplete z3 query
+# returns selection: list of all lesson slots for us to iterate through the model to find schedule
 def parseZ3Query(mods, numToTake, solver = Solver()):
-    timetable = []
-    selection = []
+    timetable = [] # contains the symbolic work hours for all mods
+    selection = [] # represents all the lecture, rec slots etc
     numMods = len(mods)
 
     # numMods choose numToTake
-    X = [Int("x_%s" % i) for i in range(numToTake)] # creates indicators determining which modules we try
+    # creates indicators determining which modules we try
+    X = [Int("x_%s" % i) for i in range(numToTake)]
+    # assert that mods are distinct and have legal indices
     solver.add(Distinct(X))
     solver.add([And(X[i] >= 0, X[i]<numMods) for i in range(numToTake)])
+
     for modIndex, mod in enumerate(mods):
         moduleCode = mod[0]
         constraints = []
         selected = Or([X[i] == modIndex for i in range(numToTake)]) # is this mod selected
+
+        # iterate through all timeslots and parse the implications
+        # Each hours of the timeslot is represented as one z3 IntSort
         for lessonType, slots in mod[1].iteritems():
             firstFlag = True
-            slotSelectors = []
+            slotSelectors = [] # selector variable for timeslot
             for slotName, timing in slots.iteritems():
                 if firstFlag:
-                    # add to timetable
-                    timetable += [Int('%s_%s_%s' % (moduleCode, lessonType[:3], index))
+                    # add to timetable, symbolic hour timeslot
+                    timetable += [Int('%s_%s_%s' % (moduleCode, lessonType, index))
                                   for index in range(len(timing))]
                     firstFlag = False
-                selector = Bool('%s_%s_%s' % (moduleCode, lessonType[:3], slotName))
+                selector = Bool('%s_%s_%s' % (moduleCode, lessonType, slotName))
                 selection.append(selector)
                 slotSelectors.append(selector)
+                # add implications if particular timeslot is selected
                 for index, time in enumerate(timing):
-                    implicants = [Int('%s_%s_%s' % (moduleCode, lessonType[:3], index)) == time]
+                    # it is fine to repeat variable names, z3 maps to the same variable
+                    implicants = [Int('%s_%s_%s' % (moduleCode, lessonType, index)) == time]
                     implication = Implies(selector, And(implicants))
                     constraints.append(implication)
+            # pick one timeslot from each timeslot, or ignore if mod is not chosen
             constraints.append(Or(Or(slotSelectors),Not(selected)))
         # not selected then we don't care, tutorial for a mod we don't choose can be at -1945024 hrs
-        # solver.add(Implies(selected, constraints))
         solver.add(constraints)
     solver.add(Or([Distinct(timetable+freeDay(i)) for i in range(5)]))
     return selection
 
-def timetableVisualizer(model, selectionMapping):
-    timings = [v for k,v in selectionMapping.iteritems() if model[k]]
-    timings = [item for sublist in timings for item in sublist]
-    timings.sort()
-
-    x = 0
-    for i in timings:
-        x |= 1 << i
-    binstr = bin(x)[2:]
-    binstr = binstr[::-1]  # reverse the string
-    print binstr
-
 def timetablePlanner(modsstr, numToTake):
+    def transformMod(modtuple):
+        return (modtuple[0], splitIntoLessonTypes(modtuple[1]))
     s = Solver()
     mods = [transformMod(query(m)) for m in modsstr]
     selection = parseZ3Query(mods, numToTake, s)
     if s.check() == sat:
         print "Candidate Timetable:"
         m = s.model()
-        for s in selection:
-            if m[s]:
-                print s
+        schedule = [str(s) for s in selection if m[s]]
     else:
         print "free day not possible"
 
@@ -132,26 +124,7 @@ def timetablePlanner(modsstr, numToTake):
 # insert unit tests here, should shift them to a separate file later
 
 def run():
-    mod = query('st2131')
-    mod = transformMod(mod)
-    # parseZ3Query([mod])
-
-    # timetablePlanner(['cs1010', 'st2131', 'cs1231', 'ma1101r', 'cs2100'])
     timetablePlanner(['cs1010', 'st2131', 'cs1231', 'ma1101r','cs2020','cs1020','cs2010'], 4)
-    # f = open('out.txt', 'w')
-    # print >> f, splitIntoLessonTypes(json.load(open('st2131.json')))
-    # f2 = open('out2.txt', 'w')
-    # print >> f2, splitIntoLessonTypes(query('st2131'))
-    # f2.close()
-
-    # f.close()
-    #lessons = splitIntoLessonTypes(mod)
-    #print lessons
-
-    #print [Bool('b%s' % i) for i in range(5)]
-
-    #print "\n\nPrinting Z3 bools"
-    #parseZ3Query([lessons])
 
 run()
 
