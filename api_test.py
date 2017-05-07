7d8fb47666e2dda74c62b8c440e7a7df3ba7d6b6
Raynold Ng <raynold.ng24@gmail.com>
2017-05-07 08:44:11 +0800

    Made API calls local for now

--- api_test.py
+++ api_test.py
@@ -3,8 +3,13 @@ from sets import Set
 import json
 from z3 import *
 
+ENV = "DEV" # faster to do everything offline
+
 
 ## Helper functions
+if ENV == "DEV":
+    _mods = json.load(open('timetable.json'))
+    _dict = {x['ModuleCode']: x['Timetable'] for x in _mods if 'Timetable' in x}
 
 # modjson: json object
 def splitIntoLessonTypes(modjson):
@@ -14,15 +19,22 @@ def splitIntoLessonTypes(modjson):
     return lessons
 
 def queryNUSMODS(code):
-    r = requests.get('http://api.nusmods.com/2014-2015/2/modules/' +  code + '.json')
-    r = r.json()
-    return r
+    # if in DEV mode then pull everything from local sources
+    if ENV == "DEV":
+        return _dict[code]
+    else:
+        r = requests.get('http://api.nusmods.com/2014-2015/2/modules/' +  code + '.json')
+        r = r.json()
+        return r
 
 # print cs1010
 # print cs1010['LessonType']
 # lessonType = Set([i for i in cs1010['LessonType']])
 
-def printJSON(obj):
-    print json.dumps(obj, indent=4, sort_keys=True)
+# def printJSON(obj):
+#    print json.dumps(obj, indent=4, sort_keys=True)
 
-print splitIntoLessonTypes(json.load(open('cs1010.json')))
+# print splitIntoLessonTypes(json.load(open('cs1010.json')))
+
+## Testing the queryNUSMODS local calls
+print queryNUSMODS('CS1010')
--- start.py
+++
@@ -1,6 +0,0 @@
-# starting code for z3
-# specific to my build
-from z3 import *
-z3_path = '/home/raynoldng/Documents/gitrepos/'
-
-init(z3_path + 'z3/build/python/libz3.so')
