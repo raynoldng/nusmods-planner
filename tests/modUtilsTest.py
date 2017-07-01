38119d0c564845943d70c3f768bdad464fa1922f
Raynold Ng <raynold.ng24@gmail.com>
2017-07-01 23:40:56 +0800

    Updated tests

--- tests/modUtilsTest.py
+++ tests/modUtilsTest.py
@@ -1,14 +1,36 @@
 import unittest
 import sys
-sys.path.append('../nusmodsplanner')
-import querySolver
-import mod_utils
+from z3 import *
+sys.path.append('../../nusmods-planner')
+from nusmodsplanner.mod_utils import CalendarUtils
 
-# TODO add tests here
 class modUtilsTest(unittest.TestCase):
 
+    def setUp(self):
+        self.CalendarUtils = CalendarUtils
 
+    def test_correctly_imported(self):
+        self.assertTrue(self.CalendarUtils != None)
+        self.assertTrue(self.CalendarUtils.gotFreeDay != None)
 
+    def test_modUtils_init(self):
+        modUtils_AY1718S1 = self.CalendarUtils('AY1718S1')
+        self.assertTrue(len(modUtils_AY1718S1._dict.keys()) > 0)
+
+        modUtils_AY1617S2 = self.CalendarUtils('AY1617S2')
+        self.assertTrue(len(modUtils_AY1617S2._dict.keys()) > 0)
+
+        self.assertTrue(len(modUtils_AY1617S2._dict.keys()) != len(modUtils_AY1718S1._dict.keys()))
+
+    def test_valid_schedule(self):
+        calendarUtils = self.CalendarUtils('AY1617S2')
+        print calendarUtils.gotFreeDay
+        testSchedule = ['GEQ1000_Tutorial_56', 'CS1010_Sectional Teaching_31', 
+                        'CS1010_Tutorial_C04', 'MA1102R_Laboratory_B11', 
+                        'MA1102R_Tutorial_T09', 'MA1102R_Lecture_SL1', 
+                        'MA1101R_Laboratory_B08', 'MA1101R_Tutorial_T17', 'MA1101R_Lecture_SL1']
+
+        self.assertTrue(calendarUtils.scheduleValid(testSchedule))
 
 if __name__ == '__main__':
     unittest.main()
--- tests/queryParserBVTests.py
+++ tests/queryParserBVTests.py
@@ -13,78 +13,106 @@ import queryParserBV
 import querySolverBV
 import mod_utils
 
+SEMESTER = 'AY1617S2'
+
 class TestQueryParserBV(unittest.TestCase):
 
-	def testOutputValid(self):
-		''' Verify that output is indeed SMTLIB2 valid syntax and can be solved
-		by a SMT solver
-		'''
-		optmods = ['CS1231', 'CS1020', 'CS2100', 'CS2020', 'CS2105', 'MA1101R', 
-				   'ST2131']
-		options = {'freeday': True}
+    def setUp(self):
+        self.calendarUtils = mod_utils.CalendarUtils(SEMESTER)
 
-	   	smtlib2, modmapping = queryParserBV.parseQuery(4, [], optmods, options)
-	   	assertions = parse_smt2_string(smtlib2)
-	   	s = Solver()
-	   	s.add(assertions)
-	   	self.assertEqual(s.check(), sat)
+    def testOutputValid(self):
+        ''' Verify that output is indeed SMTLIB2 valid syntax and can be solved
+        by a SMT solver
+        '''
+        optmods = ['CS1231', 'CS1020', 'CS2100', 'CS2020', 'CS2105', 'MA1101R', 
+                   'ST2131']
+        options = {'freeday': True}
 
-   	def testFreedayQuery(self):
-   		''' Verify that assertions represent a timetable with a freeday
+        smtlib2, modmapping = queryParserBV.parseQuery(4, [], optmods, options, semester = SEMESTER)
+        assertions = parse_smt2_string(smtlib2)
+        s = Solver()
+        s.add(assertions)
+        self.assertEqual(s.check(), sat)
 
-   		'''
-   		optmods = ['CS1231', 'CS1020', 'CS2100', 'CS2020', 'CS2105', 'MA1101R', 
-				   'ST2131']
-		options = {'freeday': True}
-		timetable = querySolverBV.solveQuery(4, [], optmods, options)
-		# print timetable
-		# print mod_utils.gotFreeDay(timetable)
-		self.assertTrue(mod_utils.scheduleValid(timetable))
-		self.assertTrue(len(mod_utils.gotFreeDay(timetable)) > 0)
+    def testFreedayQuery(self):
+        ''' Verify that assertions represent a timetable with a freeday
 
-	def testNoLessonBefore(self):
-		'''Verify that assertions represent a timetable that has no lessons
-		before the user defined time
-		'''
-		optmods = ['CS1231', 'CS1020', 'CS2100', 'CS2020', 'CS2105', 'MA1101R', 
-				   'ST2131']
-		options = {'noLessonsBefore': 12}
-		timetable = querySolverBV.solveQuery(4, [], optmods, options)
+        '''
+        optmods = ['CS1231', 'CS1020', 'CS2100', 'CS2020', 'CS2105', 'MA1101R', 
+                   'ST2131']
+        options = {'freeday': True}
+        timetable = querySolverBV.solveQuery(4, [], optmods, options, semester = SEMESTER)
+        # print timetable
+        # print mod_utils.gotFreeDay(timetable)
+        self.assertTrue(self.calendarUtils.scheduleValid(timetable))
+        self.assertTrue(len(self.calendarUtils.gotFreeDay(timetable)) > 0)
 
-		self.assertTrue(mod_utils.scheduleValid(timetable))
-		self.assertTrue(mod_utils.checkNoLessonsBefore(timetable, 12))
+    def testNoLessonBefore(self):
+        '''Verify that assertions represent a timetable that has no lessons
+        before the user defined time
+        '''
+        optmods = ['CS1231', 'CS1020', 'CS2100', 'CS2020', 'CS2105', 'MA1101R', 
+                   'ST2131']
+        options = {'noLessonsBefore': 12}
+        timetable = querySolverBV.solveQuery(4, [], optmods, options, semester = SEMESTER)
 
-	def testNoLessonAfter(self):
-		'''Verify that assertions represent a timetable that has no lessons
-		before the user defined time
-		'''
-		optmods = ['CS1231', 'CS1020', 'CS2100', 'CS2020', 'CS2105', 'MA1101R', 
-				   'ST2131']
-		options = {'noLessonsAfter': 19}
-		timetable = querySolverBV.solveQuery(4, [], optmods, options)
+        self.assertTrue(self.calendarUtils.scheduleValid(timetable))
+        self.assertTrue(self.calendarUtils.checkNoLessonsBefore(timetable, 12))
 
-		self.assertTrue(mod_utils.scheduleValid(timetable))
-		self.assertTrue(mod_utils.checkNoLessonsAfter(timetable, 19))
+    def testNoLessonAfter(self):
+        '''Verify that assertions represent a timetable that has no lessons
+        before the user defined time
+        '''
+        optmods = ['CS1231', 'CS1020', 'CS2100', 'CS2020', 'CS2105', 'MA1101R', 
+                   'ST2131']
+        options = {'noLessonsAfter': 19}
+        timetable = querySolverBV.solveQuery(4, [], optmods, options, semester = SEMESTER)
 
-	def testSatAndValidTimetable(self):
-		''' Returns empty (unsat) during test, should not be the case
-		'''
-		optmods = ['GEQ1000', 'CS1010', 'MA1102R', 'MA1101R']
-		options = {}
-		timetable = querySolverBV.solveQuery(4, [], optmods, options)
-		print timetable
-		self.assertTrue(mod_utils.scheduleValid(timetable))
-		self.assertTrue(mod_utils.checkNoLessonsAfter(timetable, 19))
+        self.assertTrue(self.calendarUtils.scheduleValid(timetable))
+        self.assertTrue(self.calendarUtils.checkNoLessonsAfter(timetable, 19))
 
-	def testSatAndValidTimetable2(self):
-		''' Returns empty (unsat) during test, should not be the case
-		'''
-		options = {'noLessonsAfter': 19, 'noLessonsBefore': 8}
-		compmods = ['MA1101R', 'ACC1002', 'GER1000', 'GEQ1000']
-		timetable = querySolverBV.solveQuery(4, compmods, [], options)
-		print timetable
-		self.assertTrue(mod_utils.scheduleValid(timetable))
-		self.assertTrue(mod_utils.checkNoLessonsAfter(timetable, 19))
+    def testSatAndValidTimetable(self):
+        ''' Returns empty (unsat) during test, should not be the case
+        '''
+        optmods = ['GEQ1000', 'CS1010', 'MA1102R', 'MA1101R']
+        options = {}
+        timetable = querySolverBV.solveQuery(4, [], optmods, options, semester = SEMESTER)
+        print timetable
+        self.assertTrue(self.calendarUtils.scheduleValid(timetable))
+        self.assertTrue(self.calendarUtils.checkNoLessonsAfter(timetable, 19))
+
+    def testSatAndValidTimetable2(self):
+        ''' Returns empty (unsat) during test, should not be the case
+        '''
+        options = {'noLessonsAfter': 19, 'noLessonsBefore': 8}
+        compmods = ['MA1101R', 'ACC1002', 'GER1000', 'GEQ1000']
+        timetable = querySolverBV.solveQuery(4, compmods, [], options, semester = SEMESTER)
+        print timetable
+        self.assertTrue(self.calendarUtils.scheduleValid(timetable))
+        self.assertTrue(self.calendarUtils.checkNoLessonsAfter(timetable, 19))
+
+    def testLockedLessonSlots1(self):
+        ''' Verify that lockedLessonSlots appear in the timetable
+        '''
+        lockedLessonSlots = ['MA1101R_Tutorial_T10']
+        compMods = ['CS1231', 'CS1020', 'CS2100', 'CS2105', 'MA1101R']
+        options = {'lockedLessonSlots': lockedLessonSlots}
+        timetable = querySolverBV.solveQuery(5, compMods, [], options, semester = SEMESTER)
+        print timetable
+        self.assertTrue(self.calendarUtils.scheduleValid(timetable))
+        self.assertTrue(lockedLessonSlots[0] in timetable)
+
+    def testLockedLessonSlots2(self):
+        ''' Verify that lockedLessonSlots appear in the timetable
+        '''
+        lockedLessonSlots = ['MA1101R_Tutorial_T10', 'CS1231_Sectional Teaching_1']
+        compMods = ['CS1231', 'CS1020', 'CS2100', 'CS2105', 'MA1101R']
+        options = {'lockedLessonSlots': lockedLessonSlots}
+        timetable = querySolverBV.solveQuery(5, compMods, [], options, semester = SEMESTER)
+        print timetable
+        self.assertTrue(self.calendarUtils.scheduleValid(timetable))
+        for slots in lockedLessonSlots:
+            self.assertTrue(slots in timetable)
 
 if __name__ == '__main__':
     unittest.main()
