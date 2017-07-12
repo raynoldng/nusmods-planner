f8227514afced8a41b80a1a0c31b389b80c981fa
Raynold Ng <raynold.ng24@gmail.com>
2017-07-12 22:30:48 +0800

    new test case

--- tests/queryParserBVTests2.py
+++ tests/queryParserBVTests2.py
@@ -1,3 +1,5 @@
+''' For AY1718S1
+'''
 import unittest
 import sys
 from z3 import *
@@ -30,5 +32,20 @@ class TestQueryParserBV(unittest.TestCase):
         # assert that we get another free day on top of the indicated Tuesday
         self.assertTrue(len(self.calendarUtils.gotFreeDay(timetable)) > 2)
 
+    def testSpecificFreedayandNoLessonsBefore(self):
+        ''' Live version returned incorrectly unsat
+        '''
+
+        compMods = ['GEQ1000', 'PS2203', 'PS2237', 'PS3271', 'SN1101E']
+        optMods = []
+        options = {'numFreedays': 2, 'freedays': ['Monday', 'Tuesday'], 'noLessonsBefore': 8}
+        numMods = 5
+
+        timetable = querySolverBV.solveQuery(numMods, compMods, optMods, options, semester = SEMESTER)
+        self.assertTrue(self.calendarUtils.scheduleValid(timetable))
+        freedays = self.calendarUtils.gotFreeDay(timetable)
+        print freedays
+        for d in ['Even Monday', 'Odd Monday', 'Even Tuesday', 'Odd Tuesday']: self.assertTrue(d in freedays)
+        print timetable
 if __name__ == '__main__':
     unittest.main()
